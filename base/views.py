057be8560276d83ed21b5d81c550324da5ef022a
Akarsh <65647232+Akarsh-N-L@users.noreply.github.com>
2023-05-24 11:43:35 +0530

    input taken using GET

--- base/views.py
+++ base/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
 
-# Create your views here.
+input=[]
+
 def home(request):
     return render(request,'home.html')
+
+def result(request):
+    freqArr=[]
+    freqs = request.GET["input"]
+    # print(type(freqs))
+    freqArr = freqs.split("%2C")
+    freqArr = freqArr[0].split(",")
+    print(freqArr)
+    for i in range(len(freqArr)):
+        input.append(float(freqArr[i]))
+    print("input=")
+    print(input)
+    print("End")
+    return render(request,'result.html', {'freqs':freqs})
